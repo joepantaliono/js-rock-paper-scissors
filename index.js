f0c1efab0a38afb2e8ffa5cd7b13857789b2237c
Joseph Pantaliono <jpanta@protonmail.com>
2021-07-11 23:40:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random()*choices.length)]; // return largest int less than or equal to random num * 3
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Draw!'
+    }
+    else if (playerSelection === 'rock' && computerSelection !== 'rock') {
+        return 'You win!'
+    }
+    else if (computerSelection === 'rock' && playerSelection !== 'rock') {
+        return 'You lose!'
+    }
+    else if (playerSelection === 'scissors' && computerSelection !== 'scissors') {
+        return 'You win!'
+    }
+    else if (computerSelection === 'scissors' && playerSelection !== 'scissors') {
+        return 'You lose!'
+    }
+}
+
+var playerSelection = 'scissors';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
