ae958a5213617e1e2fe107bf02ac8668f71fb1e5
Joseph Pantaliono <jpanta@protonmail.com>
2021-07-12 13:06:08 -0500

    added game()

---
+++ README.md
@@ -0,0 +1,5 @@
+rock paper scissors in javascript
+
+computerplay(): automatically genrates a computer's choice
+playRound(): plays a single round of rock paper scissors
+game(): automatically plays 10 rounds
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
-    return choices[Math.floor(Math.random()*choices.length)]; // return largest int less than or equal to random num * 3
+    return choices[Math.floor(Math.random()*choices.length)];
 }
-
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -22,6 +24,42 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-var playerSelection = 'scissors';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    // plays 10 random rounds and returns score
+    for (let roundCount = 0; roundCount < 10; roundCount++) {
+        let playerSelection = choices[Math.floor(Math.random()*choices.length)]; //randomly generated
+        let computerSelection = computerPlay(); //randomly generated
+        let result = playRound(playerSelection, computerSelection);
+        if (result === 'Draw') {
+            //pass
+        }
+        else if (result === 'You win!') {
+            playerScore++
+        }
+        else if (result === 'You lose!') {
+            computerScore++
+        }
+    }
+    console.log("Final Scores:");
+    console.log("Computer's score: " + computerScore);
+    console.log("Player's score: " + playerScore);
+
+    if (computerScore > playerScore) {
+        console.log('You lose the game.')
+    }
+    else if (playerScore > computerScore) {
+        console.log('You won the game!')
+    }
+    else {
+        console.log('Game ends in a draw')
+    }
+}
+
+
+
+
+
+let choices = ['rock', 'paper', 'scissors'];
+let playerSelection = choices[Math.floor(Math.random()*choices.length)];
+
+console.log(game());
